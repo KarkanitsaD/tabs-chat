26be07cd3eacff209369c2b9853a67c5b6536398
dzmitry.karkanitsa <aakarkanica@gmail.com>
2024-05-14 14:34:15 +0300

    Added functionality to send messages and put it in state.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NgxsModule } from '@ngxs/store';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +19,9 @@ import { MessagesState } from './messgaes-state/messages.state';
         AppRoutingModule,
         MatFormFieldModule,
         MatInputModule,
+        MatIconModule,
         NgxsModule.forRoot([MessagesState]),
+        ReactiveFormsModule,
     ],
     providers: [provideAnimationsAsync()],
     bootstrap: [AppComponent],
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Message } from '../../models/message.interface';
+import { MessagesState } from '../../messgaes-state/messages.state';
+import { FormControl } from '@angular/forms';
+import { SendMessage } from '../../messgaes-state/messages.actions';
 
 @Component({
     selector: 'app-chat',
@@ -6,4 +12,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrl: './chat.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChatComponent {}
+export class ChatComponent {
+    messages$: Observable<Message[]> = this.store.select(MessagesState);
+
+    messageControl = new FormControl<string>('');
+
+    constructor(private store: Store) {}
+
+    onSend(): void {
+        const content = this.messageControl.value;
+        if (content) {
+            this.store.dispatch(new SendMessage({ userId: 1, content }));
+            this.messageControl.setValue('');
+        }
+    }
+}
