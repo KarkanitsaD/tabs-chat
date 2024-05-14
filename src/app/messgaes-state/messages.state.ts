4a5af8b248e7072ff8b4d72988a32774b1059523
dzmitry.karkanitsa <aakarkanica@gmail.com>
2024-05-14 13:55:16 +0300

    Added Injectable decorator to MessagesState.

--- src/app/messgaes-state/messages.state.ts
+++ src/app/messgaes-state/messages.state.ts
@@ -2,11 +2,13 @@ import { Action, NgxsOnInit, State, StateContext } from '@ngxs/store';
 import { Message } from '../models/message.interface';
 import { SendMessage } from './messages.actions';
 import { append } from '@ngxs/store/operators';
+import { Injectable } from '@angular/core';
 
 @State<Message[]>({
     name: 'messgaes',
     defaults: [],
 })
+@Injectable()
 export class MessagesState implements NgxsOnInit {
     ngxsOnInit({ setState }: StateContext<Message[]>): void {
         const json = localStorage.getItem('messages');
